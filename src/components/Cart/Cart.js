da8a62d06ee8bd4b934168cb45725282b0712bb9
gabrielhicks <gabrielhicks@mail.com>
2020-11-06 14:22:00 -0500

    yes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
 export class Cart extends Component {
-
     renderCart = () => {
-        return this.props.cart.map(item => <h1>{item.name}</h1>)
+    return this.props.cart.map(item => (
+    <>
+    <h1>{item.name}</h1>
+    <p>image</p>
+    <p>Price</p>
+    <select></select>
+    <hr></hr>
+    </>
+    ))
     }
     render() {
         return (
             <div>
                 {this.renderCart()}
+                <button>buy</button>
             </div>
         )
     }
