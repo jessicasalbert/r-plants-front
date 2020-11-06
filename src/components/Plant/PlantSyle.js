143260a3d356c48f1a95b271ab5c0f9a4ec9d382
gabrielhicks <gabrielhicks@mail.com>
2020-11-05 22:34:27 -0500

    responsive grid

--- src/components/Plant/PlantSyle.js
+++ src/components/Plant/PlantSyle.js
@@ -1,22 +1,41 @@
 import styled from 'styled-components'
 
 const PlantStyle = styled.div`
+    h3 {
+        font-size: 100%;
+    }
     background-color: rgba(255, 255, 255, 0.75);
     border-radius: 10px;
     padding: 10px;
     margin: 10px;
     transition: 0.3s;
-    width: 175px;
-    height: 175px;
-    ${'' /* &:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    } */}
-`   
+    max-width: 20vw;
+    max-height: 100%;
 
+    @media (max-width: 900px) {
+        h3 {
+            font-size: 100%;
+        }
+        max-width: 26vw;
+        max-height: 100%;
+    }
+    @media (max-width: 800px) {
+            h3 {
+            font-size: 100%;
+        }
+        max-width: 50vw;
+        max-height: 100%;
+    }
+
+    &:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+`   
 const PlantImage = styled.img`
-    max-width: 10vw;
-    width: 10vw;
+    max-height: 100%;
+    max-width: 100%;
     display: block;
+}
 `
 
 export {PlantImage, PlantStyle}
--- src/components/PlantsContainer/PlantContainerStyle.js
+++ src/components/PlantsContainer/PlantContainerStyle.js
@@ -4,10 +4,20 @@ const PlantContainerStyle = styled.section`
     display: flex;
     flex-flow: row wrap;
     flex-direction: row;
+    justify-content: center;
     align-content: flex-start;
     margin: 10px;
     padding: 10px;
     margin: 10px;
+    @media (max-width: 900px) {
+        flex-direction: row;
+        align-content: flex-start;
+    }
+    @media (max-width: 800px) {
+        flex-direction: column;
+        align-content: center;
+    }
+}
 `
 
 export default PlantContainerStyle
