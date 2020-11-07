3fcf1d2a511b8d693d35780f40f23fdb56593219
gabrielhicks <gabrielhicks@mail.com>
2020-11-07 17:07:55 -0500

    MAPPED

--- src/components/Welcome/MapView.js
+++ src/components/Welcome/MapView.js
@@ -6,7 +6,11 @@ export class MapView extends Component {
     render() {
         return (
             <MapStyle>
-                <MapContainer></MapContainer>
+                <MapContainer center={[40.7628, -73.9800]} zoom={15}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+                </MapContainer>
             </MapStyle>
         )
     }
