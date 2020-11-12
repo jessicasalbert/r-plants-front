dc6b88f16473504e5b1bd46952bd112cbf4dc92c
gabrielhicks <gabrielhicks@mail.com>
2020-11-12 14:24:59 -0500

    no pet friendly

--- src/components/PlantsContainer/PlantContainerStyle.js
+++ src/components/PlantsContainer/PlantContainerStyle.js
@@ -35,7 +35,7 @@ const StoreWrapper = styled.div`
     background-size: cover;
     background-position: top center;
     background-repeat: no-repeat;
-    background-size: 100vw;
+    background-size: 100vh;
     @media screen and (max-width: 700px) {
         background-size: 100vh;
     }
